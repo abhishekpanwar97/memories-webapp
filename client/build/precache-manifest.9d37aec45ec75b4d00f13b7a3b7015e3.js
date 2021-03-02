3288c51077aa3cee9756aa8ffa075a2977aa8b45
abhishekpanwar971 <abhishekpanwar971@gmail.com>
2021-03-03 04:26:29 +0530

    first commit

---
+++ client/build/precache-manifest.9d37aec45ec75b4d00f13b7a3b7015e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b909fb53083a00cd37fa643c0e3b8c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "013147330ac9da40c83d",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013147330ac9da40c83d",
+    "url": "/static/js/main.50fd018d.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
